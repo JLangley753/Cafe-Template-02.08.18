954b8d2a5867a64e1fa03a5cee1461e92314a2c2
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-08-10 19:14:00 +0100

    Small amendments

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 $(function() {
-var slideIndex = 0;
-showSlides();
+  var slideIndex = 0;
+  showSlides();
 
-function showSlides() {
+  function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
+    if (slideIndex > slides.length) {
+      slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 9000);
   };
 
-  $(document).on('click', 'a[href^="#"]', function (event) {
-      event.preventDefault();
+  $(document).on('click', 'a[href^="#"]', function(event) {
+    event.preventDefault();
 
-      $('html, body').animate({
-          scrollTop: $($.attr(this, 'href')).offset().top
-      }, 800);
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
   });
 
 });
